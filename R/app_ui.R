ee2244fa23d2a724d88367937d8d38486ece3594
arytontediarjo <atediarjo@gmail.com>
2021-04-29 14:27:08 -0700

    change adjustment

--- R/app_ui.R
+++ R/app_ui.R
@@ -39,6 +39,7 @@ app_ui <- function(request) {
         fluidRow(
           br(),
           column(width = 7, 
+                 align = "center",
                  div(mod_render_image_ui("render_image_ui_1"))),
           column(width = 4,
                  offset = 1,
